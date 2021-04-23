00e142222b124e3482cde2a8a2fda6f594fc094e
Aslesha <aslesha.pokhrel@mail.utoronto.ca>
2021-04-23 19:56:32 -0400

    Use same dataset for all training

--- scripts/train_vanilla.py
+++ scripts/train_vanilla.py
@@ -4,18 +4,25 @@ import torch
 from src.Preprocessing.preprocessing import *
 from src.models.VanillaVae import VanillaVAE
 from train import train
+import numpy as np
 
-# add or remove objects here
-objects = ['mouse', 'airplane']
+# # add or remove objects here
+# objects = ['mouse', 'airplane']
 
-data_location = "./data/"
+# data_location = "./data/"
 
-# load data
-data = load_mult_data(objects,data_location)
+data_sample = "./data_sample.npy"
+data_names = "./data_names.npy"
 
-# get merged datasets
-num_samples = 2
-train_dataset, train_label = create_dataset(data, objects, num_samples)
+# # load data
+# data = load_mult_data(objects,data_location)
+
+# # get merged datasets
+# num_samples = 2
+# train_dataset, train_label = create_dataset(data, objects, num_samples)
+
+train_dataset = np.load(data_location)
+train_label = np.load(data_names)
 
 if torch.cuda.is_available():
     vanilla_vae = VanillaVAE().to(torch.device("cuda"))
