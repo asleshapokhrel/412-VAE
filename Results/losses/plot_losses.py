42734048e63803357e3ad9b7423f983844bacf82
Aslesha <aslesha.pokhrel@mail.utoronto.ca>
2021-04-24 17:11:10 -0400

    Plot losses

--- Results/losses/plot_losses.py
+++ Results/losses/plot_losses.py
@@ -1,14 +1,41 @@
 import ast
 import matplotlib.pyplot as plt
+import numpy as np
 
-f_name = "total_losses_per_epochvanilla_latent_dim_20.txt"
-with open(f_name, "r") as f:
-    loaded_list = f.read()
-    f.close()
+import seaborn as sns
+from matplotlib import rc
 
-x = ast.literal_eval(loaded_list)
+# for formatting the graph
+sns.set()
+font = {'family': 'DejaVu Sans', 'size': 14}  # adjust fonts
+rc('font', **font)
 
-plt.figure()
-plt.plot(x)
-plt.xlabel("epoch")
-plt.ylabel("loss")
+
+def load_data(f_name, total=False):
+    with open(f_name, "r") as f:
+        loaded_list = f.read()
+        f.close()
+
+    arr = ast.literal_eval(loaded_list)
+
+    if total:
+        arr = np.array(arr) / (60000 / 256)
+
+    return arr
+
+
+val = load_data("plot_val_loss_beta_4_latent4.txt")
+train = load_data("plot_total_losses_per_epochbeta_4_latent4.txt", total=True)
+
+if __name__ == "__main__":
+    plt.figure()
+    plt.plot(val, label="validation loss")
+    plt.plot(train, label="training loss")
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.title("Losses during training")
+    plt.legend()
+    plt.savefig("loss.pdf")
+    plt.show()
+
+
