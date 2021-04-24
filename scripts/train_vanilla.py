f342909a38637566f0a27007ba4fd6fc3f730af0
Utkarsh <u.agarwal@mail.utoronto.ca>
2021-04-24 01:09:18 +0000

    Results

--- scripts/train_vanilla.py
+++ scripts/train_vanilla.py
@@ -21,11 +21,11 @@ data_names = "./data_names.npy"
 # num_samples = 2
 # train_dataset, train_label = create_dataset(data, objects, num_samples)
 
-train_dataset = np.load(data_location)
+train_dataset = np.load(data_sample)
 train_label = np.load(data_names)
 
 if torch.cuda.is_available():
     vanilla_vae = VanillaVAE().to(torch.device("cuda"))
 else:
     vanilla_vae = VanillaVAE()
-train(vanilla_vae, train_dataset, 100, 256, "./vanilla.pth")
+train(vanilla_vae, train_dataset, 100, 256, "./vanilla_latent4_samedataset.pth")
